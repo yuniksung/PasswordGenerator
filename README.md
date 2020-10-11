36ebaebe7c464de336f6cd67617035c3c5fa2297
Joshua Sung <yuniksung@gmail.com>
2020-10-11 12:50:35 -0500

    completed

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Password Generator Starter Code
+# Password-Generator
+
+
+## Screenshot of the webpage in computer
+![](/assets/images/password.jpg)
+
+## Screenshot of the webpage in smaller device
+![](/assets/images/password1.jpg)
+
+[ Click here to link to Yunik's portfolio ](https://yuniksung.github.io/Yunique/)
+
+## Purpose
+
+This project was designed as a homework assignment for UTA's coding bootcamp. 
+
+This application emphasizes the use of Javascript to generate a random, secure password for the user. 
+
+### Built With
+* HTML
+* CSS
+* JavaScript
+
+---
+## Contribution
+- YUNIK SUNG <YUNIKSUNG@GMAIL.COM>
+---
+
+
+
+
--- script.js
+++ script.js
@@ -1,32 +1,77 @@
+  
 // Assignment Code
-<<<<<<< HEAD
-=======
 var enter;
 var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
+var choices = [];
 
-//Special Characters
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-//Numbers Charaters
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//Letters Characters
+character =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "+" ];;
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-space = [];
->>>>>>> assignment_code
+capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function getRandomChart(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomChar = arr[randomIndex]
+  return randomChar;
 }
 
-// Add event listener to generate button
+
+// Generate function start
+function generatePassword() {
+  var result = [];
+  enter = parseInt(window.prompt("Please pick a password length between 8 and 128 characters."));
+  if (!enter) {alert("This needs a value");}
+
+    else if (enter < 8 || enter > 128) {
+     enter = paresInt(prompt("You must choose between 8 to 128"));
+  // continue asking
+   } else {
+        // Continues once user input is validated
+        confirmNumber = confirm("Will this contain numbers?");
+        confirmCharacter = confirm("Will this contain special characters?");
+        confirmUppercase = confirm("Will this contain Uppercase letters?");
+        confirmLowercase = confirm("Will this contain Lowercase letters?");
+    };
+
+  // If user did not choose any of the options
+  if (!confirmNumber && !confirmCharacter && !confirmLowercase && !confirmUppercase) {
+    choices = alert("Sorry, this cannot generate password. Please try again");
+  }
+  if (confirmNumber) {
+    choices = choices.concat(numbers);
+  }
+  if (confirmCharacter) {
+    choices = choices.concat(character);
+  }
+  if (confirmLowercase) {
+    choices = choices.concat(letters);
+  }
+  if (confirmUppercase) {
+    choices = choices.concat(capitalLetters);
+  }
+  for (var i = 0; i < enter; i++) {
+    var randomChar = getRandomChart(choices);
+    result.push(randomChar);
+  }
+  choices = [];
+  return result.join()
+}
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+// Write password to the #password
+function writePassword() {
+  var password = generatePassword();
+  console.log(password);
+  var passwordArr = password.split(",");
+  var newPassword = "";
+  for (i = 0; i < passwordArr.length; i++) {
+    newPassword = newPassword + passwordArr[i];
+  }
+  var passwordText = document.querySelector("#password");
+  passwordText.value = newPassword;
+}
 generateBtn.addEventListener("click", writePassword);
+console.log(passwordArr);
